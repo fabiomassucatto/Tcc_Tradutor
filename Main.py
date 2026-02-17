2ed8e0ae975dcc0ea85c759c8a2716eb82a01a40
fabiomassucatto <fabiomassucatto0102@gmail.com>
2026-02-17 19:24:15 -0300

    processo

--- Main.py
+++ Main.py
@@ -1,16 +1,19 @@
 from flask import Flask, render_template, request, redirect, flash
-
+from flask_mysqldb import MySQL
 app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '123456789'
+app.config['MYSQL_DB'] = 'TCC_banco'
+
+MySQL = MySQL(app)
 
 from views.views import *
 app.register_blueprint(views)
 
 
 
-
-#@app.route('/cadastro')
-#def cadastro():
-    #return render_template("cadastro.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- views/views.py
+++ views/views.py
@@ -1,12 +1,47 @@
 from flask import Blueprint, render_template, request, redirect, url_for
+import mysql
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 views = Blueprint('views', __name__)
 
-@views.route('/login')
+@views.route('/', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    if request.method == 'POST':
+        email = request.form['email']
+        senha = request.form['senha']
+
+        cursor = mysql.connection.cursor()
+        cursor.execute("SELECT * FROM usuarios WHERE email = %s", (email,))
+        usuario = cursor.fetchone()
+        cursor.close()
+
+        if usuario and check_password_hash(usuario[3], senha):
+            return "Login realizado com sucesso!"
+        else:
+            return "Email ou senha inválidos"
+
+    return render_template('login.html')
 
 
-@views.route('/cadastro')
+@views.route('/cadastro', methods=['GET', 'POST'])
+
 def cadastro():
-    return render_template("cadastro.html")
+   if request.method == 'POST':
+        nome = request.form['nome']
+        email = request.form['email']
+        senha = request.form['senha']
+
+        senha_hash = generate_password_hash(senha)
+
+        cursor = mysql.connection.cursor()
+        cursor.execute(
+            "INSERT INTO usuarios (nome, email, senha) VALUES (%s, %s, %s)",
+            (nome, email, senha_hash)
+        )
+        mysql.connection.commit()
+        cursor.close()
+
+        return "Usuário cadastrado com sucesso!"
+
+   return render_template('cadastro.html')
